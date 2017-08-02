1554dd8c17cf1ab2435cff3a7146aa8f61c9a711
keita0-PC\keita0 <keita.clown.0420@gmail.com>
2017-08-02 17:07:37 +0900

    Javaカリキュラム　クラス　クラスの作成　課題提出いたします

---
+++ CampChallenge_java/013.クラス/Human.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keita0
+ */
+//以下の機能を持つクラスを作成してください。
+//1. パブリックな２つの変数と、２つの変数に値を設定するパブリックなメソッドと、
+//２つの変数の中身をprintするパブリックなメソッド
+
+
+public class Human {
+    public String name = "";
+    public int age = 0;
+    
+    public void setHuman(String n, int a){
+        this.name = n;
+        this.age = a;
+        
+    }
+    
+}
+
