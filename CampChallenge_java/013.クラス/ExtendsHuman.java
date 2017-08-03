1bcaa8374732db25c55820ee866930f6989ecceb
keita0-PC\keita0 <keita.clown.0420@gmail.com>
2017-08-03 16:56:02 +0900

    Javaカリキュラム　クラス　クラスの継承　課題提出いたします

---
+++ CampChallenge_java/013.クラス/ExtendsHuman.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author keita0
+ */
+
+//  課題「クラスの作成」で作成したクラスを継承し、以下の機能を持つクラスを作成してください。
+//  1. ２つの変数の中身をクリアするパブリックなメソッド
+
+
+public class ExtendsHuman extends Human {
+    public void clear (){
+        
+        
+    }
+    
+}
